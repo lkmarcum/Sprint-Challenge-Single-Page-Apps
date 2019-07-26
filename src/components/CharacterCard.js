f046de264ccdeeadd9504843f72be5c9891adc23
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-07-26 10:39:51 -0500

    created CharacterCard using semantic-ui component

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,6 +1,27 @@
 import React from "react";
+import { Card, Icon, Image } from "semantic-ui-react";
 
 export default function CharacterCard({ character }) {
-  console.log(character);
-  return <span>{character.name}</span>;
+  // image={image}
+  return (
+    <Card>
+      <Image src={character.image} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{character.name}</Card.Header>
+        <Card.Meta>
+          <span className="date">
+            {character.species} {character.status}
+          </span>
+        </Card.Meta>
+        <Card.Description>Location: {character.location.name}</Card.Description>
+        <Card.Description>Origin: {character.origin.name}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name="user" />
+          Episodes
+        </a>
+      </Card.Content>
+    </Card>
+  );
 }
